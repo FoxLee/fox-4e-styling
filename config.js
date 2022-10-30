a7d37b525de08c9315c2bb688f0d04f47562bca3
FoxLee <fox@munchlax.org>
2022-10-30 23:04:15 +1100

    Add files via upload
    - Fix to setting journal font config refresh
    - Changed all settings to client config. Why not let people choose?

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export function registerConfigs() {
 		name: `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_STYLES}.Name`,
 		default: true,
 		type: Boolean,
-		scope: 'world',
+		scope: 'client',
 		config: true,
 		hint: `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_STYLES}.Hint`,
 		onChange: debouncedReload
@@ -14,19 +14,20 @@ export function registerConfigs() {
 	name: `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_FONT}.Name`,
 		default: "DragonBodySerif",
 		type: String,
-		scope: 'world',
+		scope: 'client',
 		config: true,
 		choices: {
 			"DragonBodySerif": `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_FONT}.Serif`,
 			"DragonBodySans": `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_FONT}.Sans`
 		},
-		hint: `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_FONT}.Hint`
+		hint: `Fox4e.settings.${Fox4eStyles.SETTINGS.JOURNAL_FONT}.Hint`,
+		onChange: debouncedReload
 	});
 	game.settings.register(Fox4eStyles.ID, Fox4eStyles.SETTINGS.CHAT_STYLES, {
 		name: `Fox4e.settings.${Fox4eStyles.SETTINGS.CHAT_STYLES}.Name`,
 		default: true,
 		type: Boolean,
-		scope: 'world',
+		scope: 'client',
 		config: true,
 		hint: `Fox4e.settings.${Fox4eStyles.SETTINGS.CHAT_STYLES}.Hint`,
 		onChange: debouncedReload
@@ -35,7 +36,7 @@ export function registerConfigs() {
 		name: `Fox4e.settings.${Fox4eStyles.SETTINGS.TAH_STYLES}.Name`,
 		default: true,
 		type: Boolean,
-		scope: 'world',
+		scope: 'client',
 		config: true,
 		hint: `Fox4e.settings.${Fox4eStyles.SETTINGS.TAH_STYLES}.Hint`,
 		onChange: debouncedReload
@@ -44,7 +45,7 @@ export function registerConfigs() {
 		name: `Fox4e.settings.${Fox4eStyles.SETTINGS.CHAT_EX_STYLES}.Name`,
 		default: true,
 		type: Boolean,
-		scope: 'world',
+		scope: 'client',
 		config: true,
 		hint: `Fox4e.settings.${Fox4eStyles.SETTINGS.CHAT_EX_STYLES}.Hint`,
 		onChange: debouncedReload
