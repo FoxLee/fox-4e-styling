6c7908f541cf6ffbb257ba2938970a1f952d39d4
FoxLee <fox@munchlax.org>
2023-03-24 09:42:39 +1100

    Hide NPC flavour on autocards
    Since my "don't output flavour for auto-generated NPC chat cards" logic was reverted in the system module, I've replaced it with a simple bit of logic to collapse the flavour area on NPC chat cards by default.

---
+++ scripts/chat.js
@@ -0,0 +1,4 @@
+Hooks.on("renderChatMessage", (app, html, data) => {
+	// Collapse NPC power cards by default (because they almost never have flavour entries)
+	html.find(".power-card.NPC .card-content").hide();
+});
