78ccd4e2661828ebdec9f6abc772482f90ef1f7b
FoxLee <fox@munchlax.org>
2023-10-18 12:30:59 +1100

    Delete scripts/chat.js
    No longer needed after system 0.4.27 updates

--- scripts/chat.js
+++
@@ -1,4 +0,0 @@
-Hooks.on("renderChatMessage", (app, html, data) => {
-	// Collapse NPC power cards by default (because they almost never have flavour entries)
-	html.find(".power-card.NPC.autogen .card-content").hide();
-});
