1b65635dc1ec525ecbac87ba40c8f785e9599783
Fox "The Art Gremblin" Lee <fox@munchlax.org>
2025-02-04 19:24:32 +1100

    Fixed sheet-breaking usage bug
    Fixed a bug with my error checking code that turned graceful fails of the "usage" string builder to become sheet breaking errors. I'm very good at JavaScript.
    
    With thanks to cozycat on Discord!

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -72,3 +72,67 @@ Handlebars.registerHelper("skillWanted", function(skill,level,addLvHalf,baseline
 
 	return wanted;
 });
+
+/* "powerUse" Handlebars Helper: outputs usage HTML snippet
+*
+*  Should contain only the usage type (encounter/daily/etc) if
+*    [available uses] or [max uses] = null, [max uses] = 0,
+*    [uses per] = [null] or [""].
+*  For recharge powers, should contain rollable recharge info in
+*    the form of [Recharge x].
+*  For uses-per-interval powers, should contain uses info in the
+*    form of [x of y/interval].
+*
+*	@param {Object} powerData The "system" object of the power
+*	@returns {string} html to be printed.
+*
+/*											*/
+Handlebars.registerHelper('powerUse', function(powerData) {
+	let snippet = game.dnd4e.config.powerUseType[powerData.useType];
+	
+	let useNormalised = powerData.uses.per||"";
+	if(useNormalised == "enc"){
+		useNormalised = "encounter";
+	}else if(useNormalised == "day"){
+		useNormalised = "daily";
+	}
+	
+	try{
+		
+		// If there is no limited use interval selected, or data is conflicting, just return the usage type label.
+		if(!powerData.uses.per || ( ["day","enc"].includes(powerData.uses.per) && powerData.preparedMaxUses == 1 && useNormalised == powerData.useType ) ){
+			return `<span class="limited ${powerData.uses.per}">${snippet}</span>`;
+		}
+		
+		// If it's a recharge power, generate HTML with rollable recharge info
+		if(powerData.useType == "recharge"){
+			snippet = `<span class="recharge">`;
+			if(powerData.rechargeRoll){
+				snippet += `<a class="item-recharge rollable numerical" data-tooltip="${game.i18n.localize( 'Fox4e.TipRechargeRoll')} ${powerData.rechargeRoll} ${game.i18n.localize('Fox4e.OrHigher')}"> ${game.i18n.localize('DND4E.PowerRecharge')} <span class="dice d6">${powerData.rechargeRoll}</span></a>`;
+			}else if(powerData.rechargeCondition){
+				snippet += `<a class="item-recharge rollable" data-tooltip="${game.i18n.localize('Fox4e.TipRecharge')} ${powerData.rechargeCondition}">${game.i18n.localize('DND4E.PowerRecharge')} ${powerData.rechargeCondition}</a>`;
+			}
+			snippet += `</span>`;
+			return snippet;
+		}
+		
+		if(powerData.preparedMaxUses != 0){
+			snippet = `<span class="limited ${powerData.uses.per} uses-${powerData.preparedMaxUses}">`;
+			
+			snippet += `<span class="limit"><label class="uses autosize" for="powerData.uses.value"><input class="number" type="text" value="${powerData.uses.value}" name="powerData.uses.value" placeholder="0" /><span class="ruler">${powerData.uses.value}</span></label> of ${powerData.preparedMaxUses}</span><span class="sep">/</span>`;
+	
+			if(powerData.uses.per && powerData.uses.per != "none"){
+				snippet += `<span class="interval">${game.dnd4e.config.limitedUsePeriods[powerData.uses.per]?.label}</span>`;
+			}
+			
+			snippet += `</span>`;
+			return snippet;
+		}
+		
+		return "<!-- Could not interpret usage settings for this power -->";
+		
+	} catch(err) {
+		console.error("Usage helper spat up. Please check input data.");
+		return "<!-- Could not interpret usage settings for this power -->";
+	}
+});
